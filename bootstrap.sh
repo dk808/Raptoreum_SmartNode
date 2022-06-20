f26c70434491795aa0eb43e442ad9ca05432ed26
Carter Sullivan <63869300+CarterSullivan@users.noreply.github.com>
2022-06-20 10:00:57 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR='/raptoreum/.raptoreumcore'
-BOOTSTRAP_TAR='https://github.com/dk808/Raptoreum_SmartNode/releases/download/v1.0.0/rtm-bootstrap.tar.gz'
+BOOTSTRAP_TAR='http://185.252.234.154/boot/rtm-bootstrap.tar.gz'
 
 if [ ! -d $DIR ]; then
   mkdir -p $DIR
