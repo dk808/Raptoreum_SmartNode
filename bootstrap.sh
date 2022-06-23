762728f8b9d10ca8e66757baefab72023ded36d7
Carter Sullivan <63869300+CarterSullivan@users.noreply.github.com>
2022-06-23 10:09:47 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DIR='/raptoreum/.raptoreumcore'
-BOOTSTRAP_TAR='http://185.252.234.154/boot/rtm-bootstrap.tar.gz'
+BOOTSTRAP_TAR='https://bootstrap.raptoreum.com/bootstrap_with_indexes.tar.xz'
 
 if [ ! -d $DIR ]; then
   mkdir -p $DIR
