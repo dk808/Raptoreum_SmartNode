8923e0917aae04a91377c091c1f74dd7eb2382ae
Vibin6up <41494921+MikeR66c@users.noreply.github.com>
2023-02-19 15:23:53 -0500

    Update bootstrap.sh
    Bootstrap already has matching updated powcache.dat in it. It was suggested in discord chat that it might cause issues if your github powcache is further behind on chain than the bootstrap itself.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,10 @@
 
 DIR='/raptoreum/.raptoreumcore'
 BOOTSTRAP_TAR='https://bootstrap.raptoreum.com/bootstraps/bootstrap.tar.xz'
-POWCACHE='https://github.com/dk808/Raptoreum_SmartNode/releases/download/v1.0.0/powcache.dat'
 
 if [ ! -d $DIR ]; then
   mkdir -p $DIR
   curl -L $BOOTSTRAP_TAR | tar xJ -C $DIR
-  curl -L $POWCACHE > $DIR//powcache.dat
 else
   echo "Datadir has been detected so bootstrap will not be used..."
 fi
